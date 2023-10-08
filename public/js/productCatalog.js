6fb13099b03f738a280f96d96b310c47cc34fbb2
sebdc <sebastienmvdc@gmail.com>
2023-10-08 18:24:59 +0800

    Fixed header and other changes
    - The header is now sticky
    - Made some minor changes to the header buttons
    - Added comments to the functions in productCatalog.js

--- public/js/productCatalog.js
+++ public/js/productCatalog.js
@@ -1,50 +1,76 @@
-/*
-    - Have a default category selected
-    - Get the category id
-    - Display products if category id matches
-
-*/
 const productGrid = document.getElementById('product-grid');
+const categoryLinks = document.querySelectorAll('.category-link');
+const categoryTitle = document.getElementById('category-title');
+const pathCategory = document.getElementById('path-category');
 
-let parsedProducts;
-if( products !== null ) {
-    parsedProducts = JSON.parse(products);
-}    
 
+/*
+    ` It parses the product data from a JSON string and returns it as
+    an array of objects. This is required so we can iterate through the
+    products properly.
+*/
+function parseProducts() {
+    return JSON.parse(products);
+}
+
+/*
+    ` This function filters and displays products based on the selected
+    category. By default, the selected category is the very first category,
+    i.e., the category with the lowest categoryID.
+*/
 document.addEventListener('DOMContentLoaded', () => {
     try {
-        parsedProducts.forEach((product) => {
+        parseProducts().forEach((product) => {
             if( product.categoryID == selectedCategory ) {
                 generateProductItem(product);
             }
         });
-
-
     } catch( error ) {
         console.log( error );
     }
 });
 
-const categoryLinks = document.querySelectorAll('.category-link');
-try {
-    categoryLinks.forEach( (categoryLink) => {
+
+/*
+    ` Sets up an event listener for the category links to filter and display
+    products. When a category link is clicked, it retrieves the selected 
+    category ID and name. Afterwards, it updates the category path, category
+    title and displays the product items for the selected category.
+*/
+categoryLinks.forEach((categoryLink) => {
+    try {
         categoryLink.addEventListener('click', (e) => {
-    
             e.preventDefault();
-            selectedCategory = categoryLink.getAttribute('data-id');
-            productGrid.innerHTML = '';
     
-            parsedProducts.forEach((product) => {
+            selectedCategory = categoryLink.getAttribute('data-id');
+            const categoryName = categoryLink.getAttribute('data-name');
+
+            pathCategory.innerHTML = `${categoryName}`;
+            productGrid.innerHTML = '';
+            categoryTitle.innerHTML = `
+                <span class = "category-title">${categoryName} <span>COLLECTION</span></span>
+            `;
+    
+            parseProducts().forEach((product) => {
                 if( product.categoryID == selectedCategory ) {
                     generateProductItem(product);
                 }
             });
         })
-    });
-} catch( error ) {
-    console.log( error );
-}
+    } catch( error ) {
+        console.log(error);
+    }
+});
 
+/*
+    ` This function generates the HTML structure for a product item
+    and adds it to the productGrid. It takes a single 'product' object
+    as a parameter and constructs the product's display with its details.
+
+    The expected 'product' object contains: 
+        productID, productName, productDescription, 
+        price, stockQuantity, categoryID
+*/
 function generateProductItem( product ) {
     const productItem = document.createElement('div');
     productItem.className = 'product-item';
