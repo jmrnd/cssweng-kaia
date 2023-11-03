f528f4d6d1ad5a02204352240f793afe3db053d5
jmrnd <jamarlancemiranda@gmail.com>
2023-11-03 20:12:24 +0800

    Update customer-reviews.css

--- public/js/customer-reviews.js
+++ public/js/customer-reviews.js
@@ -15,6 +15,35 @@ const starIconHalf = `<svg class="star-icon" xmlns="http://www.w3.org/2000/svg" 
 document.addEventListener("DOMContentLoaded", function () {
     renderAverageRating();
 
+    const dropdowns = document.querySelectorAll(".dropdown");
+
+    dropdowns.forEach((dropdown) => {
+        const select = dropdown.querySelector(".select");
+        const caret = dropdown.querySelector(".caret");
+        const menu = dropdown.querySelector(".menu");
+        const options = dropdown.querySelectorAll(".menu li");
+        const selected = dropdown.querySelector(".selected");
+
+        select.addEventListener("click", () => {
+            select.classList.toggle("select-clicked");
+            caret.classList.toggle("caret-rotate");
+            menu.classList.toggle("menu-open");
+        });
+
+        options.forEach((option) => {
+            option.addEventListener("click", () => {
+                selected.innerText = option.innerText;
+                select.classList.remove("select-clicked");
+                caret.classList.remove("caret-rotate");
+                menu.classList.remove("menu-open");
+                options.forEach((option) => {
+                    option.classList.remove("active");
+                });
+                option.classList.add("active");
+            });
+        });
+    });
+
     const customerReviews = document.querySelectorAll(".customer-review");
 
     customerReviews.forEach((review) => {
--- public/js/product.js
+++ public/js/product.js
@@ -1,12 +1,11 @@
-const smallImages = document.querySelectorAll('.product-gallery-small-image');
+const smallImages = document.querySelectorAll(".product-gallery-small-image");
 
 document.addEventListener("DOMContentLoaded", function () {
-    
     let slideIndex = 1;
 
     function showSlides(n) {
         let i;
-        const slides = document.querySelectorAll('.hide');
+        const slides = document.querySelectorAll(".hide");
 
         if (n > slides.length) {
             slideIndex = 1;
@@ -17,26 +16,30 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = 'none';
+            slides[i].style.display = "none";
         }
 
-        slides[slideIndex - 1].style.display = 'block';
+        slides[slideIndex - 1].style.display = "block";
     }
 
     showSlides(slideIndex);
 
     // Event listeners for previous and next buttons
-    document.querySelector('.previous-image-button').addEventListener('click', () => {
-        plusSlides(-1);
-    });
+    document
+        .querySelector(".previous-image-button")
+        .addEventListener("click", () => {
+            plusSlides(-1);
+        });
 
-    document.querySelector('.next-image-button').addEventListener('click', () => {
-        plusSlides(1);
-    });
+    document
+        .querySelector(".next-image-button")
+        .addEventListener("click", () => {
+            plusSlides(1);
+        });
 
     // Select the first color and small image (initial)
-    const initialColorButton = document.querySelector('.color-button');
-    const initialImage = document.querySelector('.product-gallery-small-image');
+    const initialColorButton = document.querySelector(".color-button");
+    const initialImage = document.querySelector(".product-gallery-small-image");
 
     if (initialColorButton) {
         selectColor(initialColorButton);
@@ -49,22 +52,28 @@ document.addEventListener("DOMContentLoaded", function () {
     let quantity = 1;
     const minQuantity = 1;
     const maxQuantity = 10;
-    const quantitySpan = document.querySelector('.product-quantity-container span');
+    const quantitySpan = document.querySelector(
+        ".product-quantity-container span"
+    );
     quantitySpan.textContent = quantity;
 
-    document.querySelector('.quantity-button:nth-child(1)').addEventListener('click', function () {
-        if (quantity > minQuantity) {
-            quantity--;
-            quantitySpan.textContent = quantity;
-        }
-    });
+    document
+        .querySelector(".quantity-button:nth-child(1)")
+        .addEventListener("click", function () {
+            if (quantity > minQuantity) {
+                quantity--;
+                quantitySpan.textContent = quantity;
+            }
+        });
 
-    document.querySelector('.quantity-button:nth-child(3)').addEventListener('click', function () {
-        if (quantity < maxQuantity) {
-            quantity++;
-            quantitySpan.textContent = quantity;
-        }
-    });
+    document
+        .querySelector(".quantity-button:nth-child(3)")
+        .addEventListener("click", function () {
+            if (quantity < maxQuantity) {
+                quantity++;
+                quantitySpan.textContent = quantity;
+            }
+        });
 
     function plusSlides(n) {
         showSlides((slideIndex += n));
@@ -73,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     smallImages.forEach((image, index) => {
-        image.addEventListener('click', () => {
+        image.addEventListener("click", () => {
             slideIndex = index + 1;
             showSlides(slideIndex);
             selectImage(image);
@@ -82,14 +91,14 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function selectColor(button) {
-    const colorButtons = document.querySelectorAll('.color-button');
-    colorButtons.forEach(btn => btn.classList.remove('selected-color'));
-    button.classList.add('selected-color');
-    const selectedColor = document.querySelector('.selected-color');
-    selectedColor.textContent = button.getAttribute('value');
+    const colorButtons = document.querySelectorAll(".color-button");
+    colorButtons.forEach((btn) => btn.classList.remove("selected-color"));
+    button.classList.add("selected-color");
+    const selectedColor = document.querySelector(".selected-color");
+    selectedColor.textContent = button.getAttribute("value");
 }
 
 function selectImage(image) {
-    smallImages.forEach(img => img.classList.remove('selected-image'));
-    image.classList.add('selected-image');
+    smallImages.forEach((img) => img.classList.remove("selected-image"));
+    image.classList.add("selected-image");
 }
